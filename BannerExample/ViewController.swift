fbbd1e0570b4db728aa0263e6c7da705ffb7bf2e
Tim Walsh <tim.walsh@cbsinteractive.com>
2019-04-04 16:05:54 -0500

    Added status details to the view.

--- BannerExample/ViewController.swift
+++ BannerExample/ViewController.swift
@@ -20,28 +20,35 @@ import MoPubAdapter
 
 class ViewController: UIViewController, GADBannerViewDelegate {
 
-    override func viewDidLoad() {
-    super.viewDidLoad()
-    print("\n\nGoogle Mobile Ads SDK version: \(GADRequest.sdkVersion()) - 'GoogleMobileAdsMediationMoPub', '4.16.0'\n\n")
-//        setUpBanner(banner: bannerView)
-//        setUpBanner(banner: bannerViewBig)
+    @IBOutlet weak var detail: UILabel!
+    var totalRequest = 0
+    var failedRequest = 0
+    var successRequest = 0
+    var dfpBannerView: DFPBannerView = {
         var validAdSizes = [CGSize]()
         validAdSizes.append(CGSize(width: 300.0, height: 250.0))
         validAdSizes.append(CGSize(width: 320.0, height: 50.0))
-        let tempBannerView = DFPBannerView(adSize: GADAdSizeFromCGSize(validAdSizes.first!))
-        tempBannerView.validAdSizes = validAdSizes.map { NSValueFromGADAdSize(GADAdSizeFromCGSize($0)) }
-        view.addSubview(tempBannerView)
-        tempBannerView.translatesAutoresizingMaskIntoConstraints = false
+        let dfpBannerView = DFPBannerView(adSize: GADAdSizeFromCGSize(validAdSizes.first!))
+        dfpBannerView.validAdSizes = validAdSizes.map { NSValueFromGADAdSize(GADAdSizeFromCGSize($0)) }
+        return dfpBannerView
+    }()
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    print("\n\nGoogle Mobile Ads SDK version: \(GADRequest.sdkVersion()) - 'GoogleMobileAdsMediationMoPub', '4.16.0'\n\n")
+        view.addSubview(dfpBannerView)
+        dfpBannerView.translatesAutoresizingMaskIntoConstraints = false
         if #available(iOS 9.0, *) {
-            tempBannerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-            tempBannerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+            dfpBannerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            dfpBannerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         } else {
             // Fallback on earlier versions
         }
-        setUpBanner(banner: tempBannerView)
+        
+        setUpBanner(banner: dfpBannerView)
   }
 
-    func setUpBanner(banner: GADBannerView){
+    func setUpBanner(banner: DFPBannerView){
         
         banner.adUnitID = "/8264/appaw-tvguide/listings"
         banner.rootViewController = self
@@ -61,19 +68,33 @@ class ViewController: UIViewController, GADBannerViewDelegate {
         let moPubExtras = GADMoPubNetworkExtras()
         moPubExtras.privacyIconSize = 20
         request.register(moPubExtras)
-        
+        dfpBannerView.alpha = 0.0
+        setDetail()
         banner.load(request)
     }
     
+    func setDetail(){
+        detail.text = "Success(\(successRequest))\nFailed(\(failedRequest))\nTotal(\(totalRequest))"
+    }
     /// Tells the delegate an ad request loaded an ad.
     func adViewDidReceiveAd(_ bannerView: GADBannerView) {
-        print("GADBanner - adViewDidReceiveAd = \(bannerView)")
+        totalRequest = totalRequest + 1
+        successRequest = successRequest + 1
+        dfpBannerView.alpha = 1.0
+        view.backgroundColor = UIColor.green.withAlphaComponent(0.5)
+        print("\n ***** GADBanner - Success(\(successRequest)) Failed(\(failedRequest)) Total(\(totalRequest)): SUCCESS = \(bannerView) - \(dfpBannerView)\n")
+        setDetail()
     }
     
     /// Tells the delegate an ad request failed.
     func adView(_ bannerView: GADBannerView,
                 didFailToReceiveAdWithError error: GADRequestError) {
-        print("GADBanner - adView:didFailToReceiveAdWithError:\(bannerView) - \(error.localizedDescription)")
+        totalRequest = totalRequest + 1
+        failedRequest = failedRequest + 1
+        dfpBannerView.alpha = 0.0
+        view.backgroundColor = UIColor.red.withAlphaComponent(0.5)
+        print("\n ***** GADBanner - Success(\(successRequest)) Failed(\(failedRequest)) Total(\(totalRequest)): FAILED = \(bannerView) - \(dfpBannerView) - \(error.localizedDescription)\n")
+        setDetail()
     }
     
     /// Tells the delegate that a full-screen view will be presented in response
